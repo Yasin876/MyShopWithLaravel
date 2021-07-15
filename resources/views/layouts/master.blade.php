bde5ec93d67e55b07c11beb95c15cf95ef0433f8
Yasin Çimiç <yasincmc2017@gmail.com>
2021-07-15 13:06:24 +0300

    header,footer and login page done

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> @yield('title') </title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+
+    {{View::make('layouts.header')}}
+       @yield('content')
+    {{View::make('layouts.footer')}}
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+<!-- page Scripts code -->
+    @yield('scripts')
+</html>
