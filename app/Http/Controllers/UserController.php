7b733a93055f2646a41c53d470f5f7efdb096564
Yasin Çimiç <yasincmc2017@gmail.com>
2021-07-16 14:19:56 +0300

    login section is done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class UserController extends Controller
+{
+
+
+    public function login(Request $request){
+
+        $user = User::where(['email'=>$request->email])->first();
+
+        if(!$user || !Hash::check($request->password,$user->password)){
+            return "Please check your password or username";
+        }else{
+           $request->session()->put('user',$user);
+           return redirect()->route('home');
+        }
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+
+Welcome to home page
+
+
